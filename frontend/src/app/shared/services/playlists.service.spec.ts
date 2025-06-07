d40f27f46376b671546e976f7f1a48222d9f2027
romanzubriichuk <roman.zubriychuk@gmail.com>
2025-06-07 17:50:28 -0500

    Update service spec

--- frontend/src/app/shared/services/playlists.service.spec.ts
+++ frontend/src/app/shared/services/playlists.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PlaylistsService } from './playlists.service';
+import { Playlist } from '../models/playlist.model';
+import { fakeAsync, tick } from '@angular/core/testing';
+import { featuredPlaylists } from '../../../../data/mock-database';
 
 describe('PlaylistsService', () => {
   let service: PlaylistsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [PlaylistsService],
+    });
     service = TestBed.inject(PlaylistsService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should return playlists after a 2s delay', fakeAsync(() => {
+    let result: Playlist[] | undefined;
+
+    service.fetchPlaylist().subscribe((data) => {
+      result = data;
+    });
+
+    // Before delay
+    expect(result).toBeUndefined();
+
+    tick(2000); // simulate 2s passing
+
+    expect(result).toEqual(featuredPlaylists.content as Playlist[]);
+  }));
 });
