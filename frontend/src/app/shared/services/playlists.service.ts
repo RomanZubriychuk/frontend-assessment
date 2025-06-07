829e29812f48ef4342acaac7476f148a1fdaea2d
romanzubriichuk <roman.zubriychuk@gmail.com>
2025-06-07 16:18:49 -0500

    Mock data service

--- frontend/src/app/shared/services/playlists.service.ts
+++ frontend/src/app/shared/services/playlists.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+import { Playlist } from '../models/playlist.model';
+import { featuredPlaylists } from '../../../../data/mock-database';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaylistsService {
-
-  constructor() { }
+  fetchPlaylist(): Observable<Playlist[]> {
+    return of(featuredPlaylists.content as Playlist[]).pipe(delay(2000));
+  }
 }
